5dfc3f55dc0227d2c466720b1b97e392ad9fa894
jeffting <jefftingey22@gmail.com>
2019-09-24 12:07:14 -0600

    finished maybe

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>GO GROCERIES</div>
+  <div>
+    <div class="flex-cont">
+      <img
+        class="pic"
+        src="https://i-love-png.com/images/cart-icon-87ca46001c38567dba6a8887607af431-900x0.png"
+      />
+      <div class="title">GO GROCERIES</div>
+    </div>
+
+    <h2>About Our App</h2>
+    <h3>
+      <u>From the ordering groceries standpoint</u>: Go Groceries would act as a home delivery app for groceries. If you've heard of GrubHub or Uber Eats, it's the exact
+      same concept, but with groceries. Basically, you would hop on the app, select a store to order groceries from, then select all the
+      groceries you want. Within about an hour or less, the groceries would be delivered to your doorstep. You would pay a small delivery fee,
+      and given the option to leave a tip.
+    </h3>
+    <h3>
+      <u>From the delivery standpoint</u>: What sets this app apart from
+      other grocery delivery services is that the delivery people, just like GrubHub, are regular people that sign up through the app to
+      be a delivery person. It isn't the store sending out the deliveries, but rather people working through the app. If I'm a delivery
+      person, I could be at home and receive a notification that someone ordered groceries at a store near me. I can choose to pick
+      up that delivery, or ignore it. If I accept the delivery, I then go pick up the groceries for the person, and deliver them to their
+      door step. I would get paid for each delivery I make, and can choose when I work. This format reduces delivery fees, and offers more options
+      of stores that the purchaser can choose from.
+    </h3>
+    <h3>
+      Please help us out and take our survey
+      <a href="https://forms.gle/qA3R1NUmGxmojVfz8">here</a> .
+    </h3>
+    <h3>
+      If you would like to learn more, fill out your info on
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdXFgrO2pFaRaRhJWWMzBiPIMu3P-Rkb1Oi9TpYaSnUKWPDyg/viewform?usp=sf_link"
+      >this form</a>.
+    </h3>
+  </div>
 </template>
+<style>
+.title {
+  font-size: 30px;
+  align-self: flex-end;
+}
+.pic {
+  width: 100px;
+}
+.flex-cont {
+  display: flex;
+}
+</style>
